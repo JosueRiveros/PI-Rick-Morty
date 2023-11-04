85f1b6a268a3d089383fef9fc913a1ad11257563
JosueRiveros <josuesmitr@gmail.com>
2023-11-03 21:21:50 -0500

    Avance M4

--- back/src/index.js
+++ back/src/index.js
@@ -1,34 +1,6 @@
 /* server config */
-const express = require("express")
-const server = express()
-const PORT = 3001
-
-/* router */
-const { router } = require("./routes/index")
-
-
-/* middlewares */
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-       'Access-Control-Allow-Headers',
-       'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.header(
-       'Access-Control-Allow-Methods',
-       'GET, POST, OPTIONS, PUT, DELETE'
-    );
-    next();
- })
-
-
-server.use(express.json())
-
-/* Este middleware agrega a todas las rutas dentro del router "/rickandmorty". Entonces, por ejemplo: 
-si tengo una ruta en el router tipo GET y dije que se llama "/login", con este middleware ahora  pasa a llamarse: 
-"/rickandmorty/login" */
-server.use("/rickandmorty", router)
+const { server } = require("./app");
+const PORT = 3001;
 
 server.listen(PORT, () => {
     console.log("Server raised in port: " + PORT)
